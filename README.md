a4bbe6159c3593f71c07273e5223957610d72b53
Colin Atkinson <colin.william.atkinson@gmail.com>
2016-02-18 22:52:45 -0500

    Add README and LICENSE

---
+++ README.md
@@ -0,0 +1,12 @@
+File Server
+===
+A really shitty file server
+
+## Why would you make this?
+Basically, I got tired of giving my friends textbook PDFs through physical media. Also, I needed somewhere to put my R.A. Martin fanfics.
+
+## Usage
+Just clone the repo, do an `npm install`, and then `npm start`. Bam, have fun.
+
+## Contributors
+Pull requests welcome. Pls.
--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,9 @@ var storage = multer.diskStorage({
     }
 });
 
-var upload = multer({storage: storage});
+var upload = multer({
+    storage: storage
+});
 var router = express.Router();
 
 var mime = require('mime-types');
@@ -24,19 +26,26 @@ var File = mongoose.model('File');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hall Hall Server' });
+    res.render('index', {
+        title: 'Hall Hall Server'
+    });
 });
 
 router.get('/view', function(req, res, next) {
     File.find(function(err, files) {
-        if(err) return next(err);
+        if (err) return next(err);
 
-        res.render('files', {data: files, title: 'Hall Hall Server'});
+        res.render('files', {
+            data: files,
+            title: 'Hall Hall Server'
+        });
     });
 });
 
 router.get('/upload', function(req, res, next) {
-    res.render('upload', { title: 'Hall Hall Server' });
+    res.render('upload', {
+        title: 'Hall Hall Server'
+    });
 });
 
 router.post('/upload', upload.single('rando'), function(req, res, next) {
@@ -48,15 +57,19 @@ router.post('/upload', upload.single('rando'), function(req, res, next) {
     };
     var file = new File(obj);
     file.save(function(err, post) {
-        if(err) { return next(err); }
+        if (err) {
+            return next(err);
+        }
 
         res.redirect('/');
     });
 });
 
 router.get('/file/:id', function(req, res, next) {
-    File.findOne({uploadId: req.params.id}, function(err, file) {
-        if(err) return next(err);
+    File.findOne({
+        uploadId: req.params.id
+    }, function(err, file) {
+        if (err) return next(err);
 
         console.log(mime.extension(file.mime));
         // res.json(file);
